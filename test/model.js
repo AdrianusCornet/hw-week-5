b5978ccc31026b9f244885e30c04627efa831a3d
Adrianus Cornet <ardjan.cornet@gmail.com>
2019-06-29 21:48:52 +0200

    model test table

---
+++ test/model.js
@@ -0,0 +1,17 @@
+const Sequelize = require("sequelize")
+const db = require('../db')
+
+const Test = db.define(
+  'test',
+  {
+    number: {
+      type: Sequelize.INTEGER,
+      field: 'num'
+    },
+    text: {
+      type: Sequelize.STRING,
+      field: 'txt'
+    }
+  },
+  { tableName: 'test_table' }
+)
