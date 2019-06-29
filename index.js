ea53b684b96ba189c54b3be20433e3abfe7c0cd8
Adrianus Cornet <ardjan.cornet@gmail.com>
2019-06-29 15:17:50 +0200

    have a working server

--- index.js
+++ index.js
@@ -4,3 +4,8 @@ const app = express()
 const port = 4000
 
 app.listen(port, () => console.log(`lisenign on port: ${port}`))
+
+app.get('/bla', (request, response) => {
+  console.log('hallo from bla')
+  response.send('bla bla')
+})
