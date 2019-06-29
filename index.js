3ff78322efd42a662073d70ec1978ae71de829b9
Adrianus Cornet <ardjan.cornet@gmail.com>
2019-06-29 17:07:53 +0200

    connect to database

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 // inports
 const express = require('express')
+const { Client } = require('pg')
 
 // constant valus
 const port = process.env.PORT || 4000
+const connectionString = 'postgresql://postgres:week-5@localhost:5432/postgres'
 
 // init's
 const app = express()
+const client = new Client({ connectionString })
 
 // code body
 app.listen(port, () => console.log(`lisenign on port: ${port}`))
@@ -15,3 +18,5 @@ app.get('/ping', (request, response) => {
   console.log('ping pong')
   response.send('pong')
 })
+
+client.connect()
