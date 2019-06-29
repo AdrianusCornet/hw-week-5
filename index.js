6ce1b76751841a7be03e9e9bfacf4558d3769f45
Adrianus Cornet <ardjan.cornet@gmail.com>
2019-06-29 16:35:49 +0200

    make server use the provided port

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require('express')
 
 const app = express()
-const port = 4000
+const port = process.env.PORT || 4000
 
 app.listen(port, () => console.log(`lisenign on port: ${port}`))
+
 // is the sever up?
 app.get('/ping', (request, response) => {
   console.log('ping pong')
